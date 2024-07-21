69389a80084e3573969920b2ac309e30b5e0f839
suryalohit <51260818+suryalohit@users.noreply.github.com>
2024-07-21 15:40:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,30 +12,18 @@ app.debug = True
 
 @app.route('/')
 def main():
-      print("start1")
+      
       chrome_options = Options()
       chrome_options.add_argument("--no-sandbox")
-    # Important Arguments won't eun without them in Gitpod
       chrome_options.add_argument("--headless")
       chrome_options.add_argument("--disable-gpu")
-      
-      
       chrome_options.add_argument("--disable-dev-shm-usage") 
-      print("start2")
       chrome_options.add_argument('--remote-debugging-pipe')
       
-      # Setup ChromeDriver
-      print("start3")
       service = Service(ChromeDriverManager().install())
-      print("start4")
+      
       driver = webdriver.Chrome(service=service, options=chrome_options)
-      print("start5")
-      driver.get("https://www.cricbuzz.com/")
-      print("start2")
-      print(driver.title)
-      print(driver.title, flush=True)
-      print(driver.current_url)
-      print(driver.current_url, flush=True)
+     
       driver.quit()
       return '200'
 
