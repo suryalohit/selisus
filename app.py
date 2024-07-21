229321c3ead9b9a7ce2b0554ed329f042ae36c34
suryalohit <51260818+suryalohit@users.noreply.github.com>
2024-07-21 11:16:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.debug = True
 def main():
     for i in range(20):
         time.sleep(3)
-        print(f" {i} :hello")
+        print(f" {i} :hello", flush=True)
         
     
     return 'hi'
