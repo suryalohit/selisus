eba9943355e679485cbee94f631587111704e539
suryalohit <51260818+suryalohit@users.noreply.github.com>
2024-07-22 23:54:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ def main():
       chrome_options = Options()
       
       chrome_options.add_argument("--no-sandbox")
+      chrome_options.add_argument("-- see")
+    
       chrome_options.add_argument("--headless")
       chrome_options.add_argument("--disable-gpu")
       chrome_options.add_argument("--disable-dev-shm-usage") 
