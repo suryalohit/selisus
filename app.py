c0ea3035ad5166d67a9353914d1347cb0172cba9
suryalohit <51260818+suryalohit@users.noreply.github.com>
2024-07-23 10:50:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import time
-import json
-import requests
+
 
 from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from flask import Flask
+
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
