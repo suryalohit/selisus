6c4697a95677ed565dbf941eec08bebc05ff78da
suryalohit <51260818+suryalohit@users.noreply.github.com>
2024-07-23 07:46:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def main():
       chrome_options.add_argument("--disable-gpu")
       chrome_options.add_argument("--no-sandbox")
       chrome_options.add_argument("--disable-dev-shm-usage") 
+      chrome_options.add_argument('--remote-debugging-pipe')
      
       
       
